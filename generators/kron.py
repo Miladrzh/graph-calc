f58764f9332524ef7db7ff12720f5e47fb455d7d
Milad <miladrzh@gmail.com>
2021-03-16 00:00:37 -0700

    Implement shuffle random functionality

--- generators/kron.py
+++ generators/kron.py
@@ -5,45 +5,47 @@ import snap
 import numpy
 
 
-def generate_and_save_graph(node_count=None, edge_count=None, abcd=None):
-    from storage.models import GeneratedGraph
-    graph_attr = generate_graph(node_count, edge_count, abcd)
-    graph_attr['generate_method'] = 'SNAP_RMAT'
+def generate_graph(node_count, edge_count, abcd=(0.57, 0.19, 0.19, 0.05),
+                   save_in_db=True,
+                   original_indices=True,
+                   shuffled_indices=True,
+                   dir='./data/generated-graphs/'):
+    Graph = get_rmat_graph_object(node_count, edge_count, abcd)
 
-    GeneratedGraph.objects.create(**graph_attr)
-
-
-def generate_graph(node_count=None, edge_count=None, abcd=None, dir='./data/generated-graphs/'):
     from stats.stats import calc
-    if node_count is None:
-        node_count = random.randint(300, 10000)  # TODO assumption
-    if edge_count is None:
-        edge_count = random.randint(node_count, 1000000)  # TODO assumption
+    stats = calc(Graph)
 
-    if abcd is None:
-        abcd = numpy.random.random(4)
-        abcd /= abcd.sum()
+    graph_attr = {'node_count': node_count, 'edge_count': edge_count, 'generate_method': 'SNAP_RMAT', **stats}
 
-    Rnd = snap.TRnd(random.randint(1, 1000000))
     file_hash = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(16))
+    print(file_hash)
 
-    # store graph attributes in a dict
-    graph_attr = {
-        'file_hash': file_hash,
-        'node_count': node_count,
-        'edge_count': edge_count,
-    }
+    if original_indices:
+        with open(dir + file_hash + '.txt', "w+") as file:
+            for EI in Graph.Edges():
+                file.write("%d , %d\n" % (EI.GetSrcNId(), EI.GetDstNId()))
+            file.close()
+        if save_in_db:
+            from storage.models import GeneratedGraph
+            graph_attr = {**graph_attr, 'file_hash': file_hash, 'ordering': 'default'}
+            GeneratedGraph.objects.create(**graph_attr)
 
+    if shuffled_indices:
+        indices_map = numpy.random.permutation(node_count)
+        with open(dir + 'shuffled_' + file_hash + '.txt', "w+") as file:
+            for EI in Graph.Edges():
+                file.write("%d , %d\n" % (indices_map[EI.GetSrcNId()], indices_map[EI.GetDstNId()]))
+            file.close()
+        if save_in_db:
+            from storage.models import GeneratedGraph
+            graph_attr = {**graph_attr, 'file_hash': 'shuffled_' + file_hash, 'ordering': 'shuffle_random'}
+            GeneratedGraph.objects.create(**graph_attr)
+
+    del Graph
+
+
+def get_rmat_graph_object(node_count, edge_count, abcd):
+    Rnd = snap.TRnd(random.randint(1, 1000000))
     Graph = snap.GenRMat(
         node_count, edge_count, float(abcd[0]), float(abcd[1]), float(abcd[2]), Rnd)
-
-    stats = calc(Graph);
-    graph_attr = {**graph_attr, **stats}  # merge stats into graph attributes
-
-    print(file_hash)
-    with open(dir + file_hash + '.txt', "w+") as file:
-        for EI in Graph.Edges():
-            file.write("%d , %d\n" % (EI.GetSrcNId(), EI.GetDstNId()))
-        file.close()
-    del Graph
-    return graph_attr
+    return Graph
