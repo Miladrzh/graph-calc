fc7964d47b0284142ee8f7859b89fcffa6fdd38b
Your Name <you@example.com>
2021-03-21 19:54:02 +0000

    sample nodes proportional to outdegree; persist results to workload model

--- workloads/test.py
+++ workloads/test.py
@@ -1,4 +1,7 @@
 import workloads.get_khop.GetKHopBenchmark as B
+
+from storage.models import WorkloadResult
+
 import numpy as np
 from generators import kron
 import os
@@ -10,12 +13,11 @@ def run_test():
     ns = [2**i for i in np.linspace(10, 20, 11)]
     ns = [i*1000 for i in range(1,10)]
     ns = [1000, 5000, 10000, 50000, 100000]
-    ns = [50000]
-
+    ns = [50000, 100000]
     params = {}
     for n in ns:
         params[n] = []
-        for factor in [2, 5, 15, 25, 50]:
+        for factor in [2, 5, 15,]:
             params[n].append(n*factor)
     print(params)
     # generate a set of increasingly bigger graphs
@@ -23,74 +25,68 @@ def run_test():
     # for n_nodes, n_edges_list in params.items():
     #     for n_edges in n_edges_list:
     #         print(n_nodes, n_edges)
-    #         kron.generate_and_save_graph(n_nodes, n_edges, 
+    #         kron.generate_graph(n_nodes, n_edges, 
     #         abcd=[.57, .19, .19])
     # return
     # get all the graph currently in db
     from storage.models import GeneratedGraph
-    all_entries = GeneratedGraph.objects.filter(file_hash__exact="u4i9ww0jg52ixxgu")
-    props = [(e.file_hash, e.node_count, e.edge_count) for e in all_entries]
-    print(props)
-    prop = props[0]
-    path = os.path.join('./data', 'generated-graphs',
-            prop[0] + '.txt')
-    print(f"running benchmark on {path}")
-    node_count = prop[1]
-    edge_count = prop[2]
-    bmark = B.GetKHopBenchmark(node_count, path, 3, "vid", 1)
-
-    bmark.runExperiment()
-    bmark.calcStats()
-    print(bmark.nSinks)
-    print(bmark.nIncomps / bmark.nExpts)
-    print(bmark.nSeenAll / bmark.nExpts)
-
-    return
-
+    all_entries = GeneratedGraph.objects.filter(edge_count__lte=1500000).filter(ordering='default')
+    props = [(e.file_hash, e.node_count, e.edge_count, e.OutDegCnt_std, e.clust_coef) for e in all_entries]
     orders = ['vid', 'random', 'desc_deg']
     i = 0
     ks = [2,3,]
-    columns = ['node_count', 'edge_count', 'order', 'k', 'mean', 'stdev', 
-                'n_incompletes', 'n_seen_all', 'n_sinks']
-
+    ks = [2,]
+    columns = ['node_count', 'edge_count', 'order', 'k', 'mean', 'stdev', 'n_incompletes', 'n_seen_all', 'n_sinks', 'out_deg_std', 'clust_coef']
+    order = 'vid'
     df = pd.DataFrame(columns=columns)
     
-    # take the first 10 hashes and run varied benchmarks on them 
-    for prop in props[1:2]:
+    # ignore shuffled files for now
+    nSamples = 100
+    print(props)
+    for prop, obj in zip(props[:], all_entries):
         d = {}
+        file_hash = prop[0] 
         path = os.path.join('./', 'data', 'generated-graphs',
-                prop[0] + '.txt')
+                file_hash + '.txt')
         print(f"running benchmark on {path}")
         node_count = prop[1]
         edge_count = prop[2]
-        
-        for order in orders:
-            for k in ks:
-                # create the benchmark
-                bmark = B.GetKHopBenchmark(node_count, path, k, order, 1)
-                bmark.runExperiment()
-                bmark.calcStats()
+        out_deg_std = prop[3]
+        clust_coef = prop[4]
+    
+        for k in ks:
+            print("node_count: " + str(node_count))
+            print("edge_count: " + str(edge_count))
+            print("k: " + str(k))
+            # create the benchmark
+            bmark = B.GetKHopBenchmark(node_count, path, k, order, 10, nSamples)
+            bmark.runExperiment()
+            bmark.calcStats()
+            d['node_count'] = node_count
+            d['edge_count'] = edge_count
+            d['order'] = order
+            d['k'] = k
+            d['mean'] = bmark.mean
+            d['stdev'] = bmark.stdev
+            d['n_incompletes'] = bmark.nIncomps / bmark.nExpts
+            d['n_seen_all'] = bmark.nSeenAll / bmark.nExpts
+            d['n_sinks'] = bmark.nSinks
+            d['out_deg_std'] = out_deg_std
+            d['clust_coef'] = clust_coef
+            # get the benchmark stats
+            df = df.append(d, ignore_index=True)
 
-                print(bmark.order)
-                print(k)
-                print(bmark.mean)
-                print(bmark.nSinks)
-                print(bmark.nIncomps / bmark.nExpts)
-                print(bmark.nSeenAll / bmark.nExpts)
-                d['node_count'] = node_count
-                d['edge_count'] = edge_count
-                d['order'] = order
-                d['k'] = k
-                d['mean'] = bmark.mean
-                d['stdev'] = bmark.stdev
-                d['n_incompletes'] = bmark.nIncomps / bmark.nExpts
-                d['n_seen_all'] = bmark.nSeenAll / bmark.nExpts
-                d['n_sinks'] = bmark.nSinks
-                # get the benchmark stats
-                df = df.append(d, ignore_index=True)
-                del bmark
+            # write to workload result model
+            WorkloadResult.objects.create(
+                file_hash=obj, 
+                experiment='2HOP',
+                duration_var=-1, 
+                duration_avg=bmark.mean, 
+                duration_std=bmark.stdev
+            )
+            del bmark
         # i+=1
         # if i>1:
             # break
 
-    df.to_csv('tmp.csv')
+    df.to_csv('./tmp.csv')
