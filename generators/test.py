4452b1d3cb7779120717d54a95744880ee68cba2
alextrosta <alextrosta@gmail.com>
2021-04-19 11:45:42 -0700

    rmat sanity test

--- generators/test.py
+++ generators/test.py
@@ -3,7 +3,8 @@ from scipy.special import comb
 from generators import kron
 from stats.stats import get_hist_array
 import json
-
+import pandas as pd
+import os
 def expected_n_vs_w_degree(k, n_edges, n, p=0.76):
     """calculate the expected number of nodes with in/out degree k
     
@@ -32,31 +33,49 @@ def expected_n_vs_w_degree(k, n_edges, n, p=0.76):
 
 def run_test():
     # generate a set of graphs
-    ns = [2**i for i in range(10,14)]
-    es = [2**i for i in range(11,15)]
+    # ns = [2**i for i in range(10,14)]
+    # es = [2**i for i in range(11,15)]
     max_k = 20
 
-    for n_nodes, n_edges in zip(ns, es):
-        n = int(np.log2(n_nodes))
-        abcd=(0.57, 0.19, 0.19, 0.05)
-        Graph = kron.get_rmat_graph_object(n_nodes, n_edges, abcd=abcd)
-        OutDegCnt = get_hist_array(Graph.GetOutDegCnt())
-        a = abcd[0]
-        b = abcd[1]
-        c = abcd[2]
-        res = {}
+    # generate 100 graphs with 1024 nodes and 1024 edges
+    ns = [1024]
+    es = [1024]
+    n_expts = 100
 
-        for i in range(OutDegCnt.shape[0]):
-            k = OutDegCnt[i][0]
-            if k > max_k:
-                break
-            else:
-                actual_n = OutDegCnt[i][1]
-                exp_n = expected_n_vs_w_degree(k, n_edges, n, p=a+b)
-                res[str(k)] = {
-                    'actual': int(actual_n),
-                    'expected': float(exp_n)
-                }
+    # persist expected and actual degree counts to csv
+    columns = ['experiment', 'k', 'expected', 'actual']
+    df = pd.DataFrame(columns=columns)
+    path = os.path.join('.', 'data', 'results', 'data_qual.csv')
+    for expt_num in range(n_expts):
+        for n_nodes, n_edges in zip(ns, es):
+            n = int(np.log2(n_nodes))
+            abcd=(0.57, 0.19, 0.19, 0.05)
+            Graph = kron.get_rmat_graph_object(n_nodes, n_edges, abcd=abcd)
+            OutDegCnt = get_hist_array(Graph.GetOutDegCnt())
+            a = abcd[0]
+            b = abcd[1]
+            c = abcd[2]
+            res = {}
 
-        
-        print(json.dumps(res, sort_keys=True, indent=4))
+            for i in range(OutDegCnt.shape[0]):
+                k = OutDegCnt[i][0]
+                if k > max_k:
+                    break
+                else:
+                    actual_n = OutDegCnt[i][1]
+                    exp_n = expected_n_vs_w_degree(k, n_edges, n, p=a+b)
+                    res[str(k)] = {
+                        'actual': int(actual_n),
+                        'expected': float(exp_n)
+                    }
+                    d = {
+                        'experiment': str(expt_num+1),
+                        'k': k,
+                        'expected': float(exp_n),
+                        'actual': int(actual_n)
+                    }
+                    df = df.append(d, ignore_index=True)
+    print(path)
+
+    df.to_csv(path)
+            # print(json.dumps(res, sort_keys=True, indent=4))
